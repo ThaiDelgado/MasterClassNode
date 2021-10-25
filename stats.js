13d726b12fab965a08403a3bdd427ffd9a94d165
ThaiDelgado <thaissadelgado@gmail.com>
2021-10-25 17:34:33 -0300

    Using setInterval

--- stats.js
+++ stats.js
@@ -1,12 +1,26 @@
+const { table } = require('console');
 const os = require('os');
 
 //const freemem = os();
 //const totalmem = os();
 
-const{freemem, totalmem} = os; //desestruturação
+setInterval(() => {
+
+    const{freemem, totalmem} = os; //desestruturação
 
 const free = parseInt(freemem() / 1024 / 1024)
 const total = parseInt(totalmem() / 1024 / 1024)
+const percents = parseInt((free/total) * 100) 
 
-console.log(`${free} MB ${total} MB`)
+const stats = {
+    free : `${free} MB`,
+    total : `${total} MB`,
+    percents : `${percents}%`
+}
+
+console.clear()
+console.log('### PC STATS ###')
+console.table(stats)
+
+}, 1000)
 
