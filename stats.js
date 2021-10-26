2839b8637b87d598ace5b9230e4a2e799a214937
ThaiDelgado <thaissadelgado@gmail.com>
2021-10-25 21:45:30 -0300

    using os.platform

--- stats.js
+++ stats.js
@@ -6,21 +6,25 @@ const os = require('os');
 
 setInterval(() => {
 
-    const{freemem, totalmem} = os; //desestruturação
+    const{freemem, totalmem, platform} = os; //desestruturação
 
-const free = parseInt(freemem() / 1024 / 1024)
-const total = parseInt(totalmem() / 1024 / 1024)
-const percents = parseInt((free/total) * 100) 
+    const free = parseInt(freemem() / 1024 / 1024)
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const percents = parseInt((free/total) * 100) 
+    const plataform = platform()
 
-const stats = {
-    free : `${free} MB`,
-    total : `${total} MB`,
-    percents : `${percents}%`
-}
 
-console.clear()
-console.log('### PC STATS ###')
-console.table(stats)
+    const stats = {
+        free : `${free} MB`,
+        total : `${total} MB`,
+        percents : `${percents}%`,
+        plataform : `${plataform}`
+    }
+
+    console.clear()
+    console.log('### PC STATS ###')
+    console.table(stats)
+
 
 }, 1000)
 
