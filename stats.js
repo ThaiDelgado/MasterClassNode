c1c0d4921c0d04144feee0f5ccae9754585b1ae6
ThaiDelgado <thaissadelgado@gmail.com>
2021-10-25 21:52:06 -0300

    Using uptime

--- stats.js
+++ stats.js
@@ -1,24 +1,27 @@
 const { table } = require('console');
 const os = require('os');
+const { uptime } = require('process');
 
 //const freemem = os();
 //const totalmem = os();
 
 setInterval(() => {
 
-    const{freemem, totalmem, platform} = os; //desestruturação
+    const{freemem, totalmem, platform, uptime} = os; //desestruturação
 
     const free = parseInt(freemem() / 1024 / 1024)
     const total = parseInt(totalmem() / 1024 / 1024)
     const percents = parseInt((free/total) * 100) 
     const plataform = platform()
+    const uptimee = uptime()  //Retorna o tempo de atividade do sistema em segundos.
 
 
     const stats = {
         free : `${free} MB`,
         total : `${total} MB`,
         percents : `${percents}%`,
-        plataform : `${plataform}`
+        plataform : `${plataform}`,
+        uptime : `${uptimee}`
     }
 
     console.clear()
